64593c642cd65d886bec45e2319f4a0f1c449fdf
Srinivas Naika <srinivas.naika@publicissapient.com>
2019-12-10 12:42:09 +0530

    updated src/test in runner

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -13,7 +13,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src\\main\\test\\Feature",
+		features = "src\\test\\java\\Feature",
 		glue = {"StepDefn","Hooks"},
 		plugin = { "pretty", "html:target/cucumber", "json:target/cucumber.json" },
 		dryRun = false,
